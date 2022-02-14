1718f9896e01d8be9c041d4965646dfbd7f41eb8
Siah-All <mateusz.placzek@gmail.com>
2022-02-14 08:51:46 +0100

    add prisma configuration

---
+++ jest.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from '@jest/types';
+
+const ignoredPatterns = [
+  '/node_modules/',
+  '/dist/',
+  '<rootDir>/volumes/',
+  '<rootDir>/prisma/',
+];
+
+const config: Config.InitialOptions = {
+  roots: ['<rootDir>'],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testPathIgnorePatterns: ignoredPatterns,
+  watchPathIgnorePatterns: ignoredPatterns,
+  testMatch: [`<rootDir>/__tests__/*.test.ts`],
+  moduleDirectories: ['<rootDir>/node_modules'],
+};
+
+export default module.exports = config;
